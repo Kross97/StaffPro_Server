caf6f84082e1c01c186f0348329561a1b217b147
kross78080@gmail.com <kross78080@yandex.ru>
2022-08-23 14:26:18 +0300

    добавление заголовков для кук

--- helpers/acceptCors.ts
+++ helpers/acceptCors.ts
@@ -1,10 +1,14 @@
 import {IncomingMessage, ServerResponse} from "http";
 
 const corsSuccess = (response, origin) => {
+    // заголовки для отключение CORS
     response.setHeader('Access-Control-Allow-Origin', origin);
     response.setHeader('Access-Control-Allow-Methods', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
     response.setHeader('Access-Control-Max-Age', 2592000);
+    // заголовки для разрешения передачи кук
+    response.setHeader('Access-Control-Allow-Credentials', 'true');
+    response.setHeader('Set-Cookie', 'myCookie=ddddddsadasd; SameSite=None; Secure')
 }
 const allowCors = ['http://localhost:3000'];
 
